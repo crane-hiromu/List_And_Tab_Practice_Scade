27a48f9bc4c839e760afb619a766367ff317b342
h.crane <h.crane.t@gmail.com>
2020-01-14 03:24:11 +0900

    append nav

--- src/main.page.swift
+++ src/main.page.swift
@@ -1,9 +1,30 @@
 import ScadeKit
 
-class MainPageAdapter: SCDLatticePageAdapter {
+// MARK: - Adapter
 
-	// page adapter initialization
+final class MainPageAdapter: SCDLatticePageAdapter {
+
+	// MARK: Properties
+	
+	#if(os(iOS))
+	private lazy var statusVar: SCDLayoutGridData? = {
+		let widget = self.page?.getWidgetByName("viewareaStatusbar")
+		let container = widget as? SCDWidgetsContainer
+		let grid = widget?.layoutData as? SCDLayoutGridData
+		return grid
+	}()
+	#endif
+	
+	
+	// MARK: Overrides
+	
 	override func load(_ path: String) {		
 		super.load(path)
+		
+		#if(os(iOS))
+		statusVar?.isExclude.toggle()
+		#endif
+		
+		
 	}
 }
