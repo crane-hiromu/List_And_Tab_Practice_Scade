bc5e4c205aa9f112f8da449935d4dd7fe678026c
h.crane <h.crane.t@gmail.com>
2020-01-15 04:47:57 +0900

    append list data

--- src/main.page.swift
+++ src/main.page.swift
@@ -15,6 +15,22 @@ final class MainPageAdapter: SCDLatticePageAdapter {
 	}()
 	#endif
 	
+	private lazy var listControl: SCDWidgetsList? = {
+	   let widget = self.page?.getWidgetByName("mainList")
+	   let list = widget as? SCDWidgetsList
+//	   list?.items = ["hoge", "huga", "fizz", "buzz"]
+//	   list?.elements
+//	   list?.template.element.children
+	   return list
+	}()
+	
+//	private lazy var row: SCDWidgetsRowView? = {
+//	   let widget = self.page?.getWidgetByName("mainListRow")
+//	   let row = widget as? SCDWidgetsRowView
+//	   return row
+//	}()
+
+	private var viewModel: MainPageViewModel?
 	
 	// MARK: Overrides
 	
@@ -24,7 +40,30 @@ final class MainPageAdapter: SCDLatticePageAdapter {
 		#if(os(iOS))
 		statusVar?.isExclude.toggle()
 		#endif
-		
-		
+	}
+	
+	override func show(_ view: SCDLatticeView?) {
+		super.show(view)
+	
+		listControl?.items = ["hoge", "huga", "fizz", "buzz", "hoge", "huga", "fizz", "buzz", "hoge", "huga", "fizz", "buzz"]
+		listControl?.elements.enumerated().forEach { i, row in
+			let label = ((row.children.first as? SCDWidgetsListView)?.children.first as? SCDWidgetsRowView)?.children.first
+			(label as? SCDWidgetsLabel)?.text = listControl?.items[i] as? String ?? ""
+			
+//			print("---element0", (($0.children[0] as? SCDWidgetsListView)?.children[0] as? SCDWidgetsRowView)?.children)
+//			print("---element1", (($0.children[0] as? SCDWidgetsListView)?.children[1] as? SCDWidgetsRowView)?.children)
+		}
+	}
+}
+
+final class MainPageViewModel: EObject {
+	let txt: String
+	
+	init(txt: String) {
+		self.txt = txt
+	}
+	
+	convenience init(txt2: String) {
+		self.init(txt: txt2)
 	}
 }
